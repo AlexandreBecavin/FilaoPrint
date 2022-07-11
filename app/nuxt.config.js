3ec51c9117989d08d6ab65399dbbd1db0827c01c
Alexandre BECAVIN <alexandre.becavin@odiwi.com>
2022-07-11 15:54:19 +0200

    mise en place de la pwa

--- app/nuxt.config.js
+++ app/nuxt.config.js
@@ -32,19 +32,41 @@ export default {
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
+    '@nuxtjs/pwa'
   ],
 
+  mode: 'universal',
+  render: {
+    ssr: false
+  },
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-  ],
+  modules: [],
+
+  pwa: {
+    icon: { purpose: 'maskable' },
+    meta:{
+      title:'Filao Print',
+      author:'Filao Print',
+    },
+    manifest: {
+      lang: 'fr',
+      name: "Filao Print",
+      short_name: "Filao Print App",
+      display: 'standalone',
+      theme_color: '#2C3C4B',
+      start_url: '/'
+    },
+    workbox: {
+      dev: true // or use a global variable to track the current NODE_ENV, etc to determine dev mode
+    }
+  },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
